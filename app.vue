4c61749857ca70b5071f3df7251cde9562fb5f7d
Paolo Ráez <str237@gmail.com>
2024-06-19 21:23:24 +0100

    Make game interactive

--- app.vue
+++ app.vue
@@ -1,17 +1,36 @@
 <script setup lang="ts">
-import { Queens } from './src/Queens'
+import { Queens, CellState, type Cell } from './src/Queens'
 import { Colors } from './src/colors'
 
 const q = new Queens({ dim: 8, })
+const board = reactive(q.board)
+
+const CellToTextMapping: Record<CellState, string> = {
+  [CellState.UNKNOWN]: '',
+  [CellState.QUEEN]: 'Q',
+  [CellState.FREE]: 'x',
+}
+
+const getNextState = (state: CellState): CellState => {
+  let next = state + 1;
+  if (next > 1) {
+    next = -1;
+  }
+  return next
+}
+
+const onClick = (cell: Cell) => {
+  cell.status = getNextState(cell.status)
+}
 
 </script>
 
 <template>
   <div class="main-container">
-    <div v-for="(row, i) in q.board" :key="i" class="row">
-      <div v-for="(cell, j) in row" :key="j" class="cell" :style="{ backgroundColor: Colors[cell.qid].value }">
-        <!-- {{ cell.isQueen ? 'Q' : '.' }} -->
-        {{ cell.qid }}
+    <div v-for="(row, i) in board" :key="i" class="row">
+      <div v-for="(cell, j) in row" :key="j" class="cell" :style="{ backgroundColor: Colors[cell.qid].value }"
+        @click="onClick(cell)">
+        {{ CellToTextMapping[cell.status] }}
       </div>
     </div>
   </div>
